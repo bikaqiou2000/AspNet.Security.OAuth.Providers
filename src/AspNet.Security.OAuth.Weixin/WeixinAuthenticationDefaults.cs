9cf47074c7e15ced9279fa96520f162cf41aca72
bikaqiou2000 <bikaqiou2000@sina.com>
2018-11-28 20:24:57 +0800

    v1

---
+++ src/AspNet.Security.OAuth.Weixin/WeixinAuthenticationDefaults.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace AspNet.Security.OAuth.Weixin
+{
+    /// <summary>
+    /// Default values for Weixin authentication.
+    /// </summary>
+    public static class WeixinAuthenticationDefaults
+    {
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.Name"/>.
+        /// </summary>
+        public const string AuthenticationScheme = "Weixin";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationScheme.DisplayName"/>.
+        /// </summary>
+        public const string DisplayName = "微信";
+
+        /// <summary>
+        /// Default value for <see cref="RemoteAuthenticationOptions.CallbackPath"/>.
+        /// </summary>
+        public const string CallbackPath = "/signin-weixin";
+
+        /// <summary>
+        /// Default value for <see cref="AuthenticationSchemeOptions.ClaimsIssuer"/>.
+        /// </summary>
+        public const string Issuer = "Weixin";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.AuthorizationEndpoint"/>.
+        /// </summary>
+        //public const string AuthorizationEndpoint = "https://open.weixin.qq.com/connect/qrconnect";
+        //企业微信的oauth登录
+        public const string AuthorizationEndpoint = "https://open.weixin.qq.com/connect/oauth2/authorize";
+        
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.TokenEndpoint"/>.
+        /// </summary>
+        //public const string TokenEndpoint = "https://api.weixin.qq.com/sns/oauth2/access_token";
+        //企业微信获取API调用令牌
+        public const string TokenEndpoint = "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
+
+        /// <summary>
+        /// Default value for <see cref="OAuthOptions.UserInformationEndpoint"/>.
+        /// </summary>
+        //public const string UserInformationEndpoint = "https://api.weixin.qq.com/sns/userinfo";
+        //企业微信获用户信息
+        public const string UserInformationEndpoint = "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo";
+        
+    }
+}
---
+++ src/AspNet.Security.OAuth.Weixin/WeixinAuthenticationOptions.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using static AspNet.Security.OAuth.Weixin.WeixinAuthenticationConstants;
+
+namespace AspNet.Security.OAuth.Weixin
+{
+    /// <summary>
+    /// Defines a set of options used by <see cref="WeixinAuthenticationHandler"/>.
+    /// </summary>
+    public class WeixinAuthenticationOptions : OAuthOptions
+    {
+        public WeixinAuthenticationOptions()
+        {
+            ClaimsIssuer = WeixinAuthenticationDefaults.Issuer;
+            CallbackPath = new PathString(WeixinAuthenticationDefaults.CallbackPath);
+
+            AuthorizationEndpoint = WeixinAuthenticationDefaults.AuthorizationEndpoint;
+            TokenEndpoint = WeixinAuthenticationDefaults.TokenEndpoint;
+            UserInformationEndpoint = WeixinAuthenticationDefaults.UserInformationEndpoint;
+
+            Scope.Add("snsapi_login");
+            Scope.Add("snsapi_userinfo");
+
+            ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "unionid");
+            ClaimActions.MapJsonKey(ClaimTypes.Name, "nickname");
+            ClaimActions.MapJsonKey(ClaimTypes.Gender, "sex");
+            ClaimActions.MapJsonKey(ClaimTypes.Country, "country");
+
+            //ClaimActions.MapJsonKey(Claims.OpenId, "openid");
+            ClaimActions.MapJsonKey(Claims.OpenId, "UserId"); //先替换成UserId
+
+            ClaimActions.MapJsonKey(Claims.Province, "province");
+            ClaimActions.MapJsonKey(Claims.City, "city");
+            ClaimActions.MapJsonKey(Claims.HeadImgUrl, "headimgurl");
+
+            ClaimActions.MapJsonKey(Claims.UserId, "UserId");
+            ClaimActions.MapJsonKey(Claims.DeviceId, "DeviceId");
+
+            ClaimActions.MapCustomJson(Claims.Privilege, user =>
+            {
+                var value = user.Value<JArray>("privilege");
+                if (value == null)
+                {
+                    return null;
+                }
+                return string.Join(",", value.ToObject<string[]>());
+            });
+
+
+        }
+    }
+}
